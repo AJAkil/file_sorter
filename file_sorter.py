74d26f8adc9d23fd62b1e6830f14c5ba15a12a14
Akil <cyclosis47@gmail.com>
2020-03-21 19:56:50 +0600

    created two functions only, no body

---
+++ file_sorter.py
@@ -0,0 +1,19 @@
+"""
+a function to create dictionary based on the type of files
+"""
+def create_type_dict():
+    pass
+
+
+
+"""
+a function to sort files and put them to their respective folders based on extensions
+"""
+def sort_files():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    pass
